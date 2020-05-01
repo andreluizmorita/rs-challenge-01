47eb2b0cc12112c9073a77594d52cc21f71cda6a
André Morita <andreluizmorita@gmail.com>
2020-05-01 14:22:59 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const cors = require('cors');
+const { uuid, isUuid } = require('uuidv4');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get('/repositories', (request, response) => {
+  return response.json(repositories);
+});
+
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository)
+
+  return response.json(repository);
+});
+
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response
+      .status(400)
+      .json( { "error": "Repository not found."})
+  }
+
+  const repository = {
+    ...repositories[repoIndex],
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repoIndex] = repository;
+
+  return response.json(repository);
+});
+
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repositoryIndex < 0) {
+    return response
+      .status(400)
+      .json( { "error": "Repository not found."})
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).json(repositories);
+});
+
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response
+      .status(400)
+      .json( { "error": "Repository not found."})
+  }
+
+  const repository = {
+    ...repositories[repoIndex],
+    likes: ++repositories[repoIndex].likes
+  }
+
+  repositories[repoIndex] = repository;
+
+  return response.json(repository);
+});
+
+module.exports = app;
